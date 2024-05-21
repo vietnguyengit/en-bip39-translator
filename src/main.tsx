d618f7db79cc58b7f0339733cf836eb3f04ed6b4
Viet Nguyen <vh.nguyen@utas.edu.au>
2024-05-21 19:14:15 +1000

    Update vite build config

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+const container = document.getElementById('root');
+
+// Type assertion to ensure container is not null
+if (container) {
+    const root = createRoot(container);
+    root.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+} else {
+    console.error("Root container is missing in the HTML file.");
+}
