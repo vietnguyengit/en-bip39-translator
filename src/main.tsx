8a06d0c76b9a0cca354c9fb3e51abb57448c63f3
Viet Nguyen <vietnguyenswin@outlook.com>
2024-05-21 20:20:41 +1000

    Update github pages deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+    throw new Error("No element with id 'root' found");
+}
+
+const root = createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+      <App />
+  </React.StrictMode>
 );
