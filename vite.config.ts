9b629c342ac67360ce3a3d31718f8a87f79b581f
Viet Nguyen <vh.nguyen@utas.edu.au>
2024-05-21 19:39:29 +1000

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/en-bip39-translator/',
+  plugins: [
+    react()
+  ],
   build: {
-    outDir: 'dist'
+    outDir: "dist",
   },
-  base: '/en-bip39-translator/'
+  publicDir: "public",
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
 })
